a9ef5bca3547395c425f72cb01c5195f0c9ce1d9
Alex Costa Machado <alexcmachado@gmail.com>
2021-12-08 16:05:19 -0300

    Create model_fn

--- src/main.py
+++ src/main.py
@@ -12,6 +12,35 @@ from train import train
 from loaders import get_loaders
 
 
+def model_fn(model_dir):
+    """Load the PyTorch model from the `model_dir` directory."""
+    print("Loading model.")
+
+    # First, load the parameters used to create the model.
+    model_info = {}
+    model_info_path = os.path.join(model_dir, "model_info.pth")
+    with open(model_info_path, "rb") as f:
+        model_info = torch.load(f)
+
+    print("model_info: {}".format(model_info))
+
+    # Determine the device and construct the model.
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = models.vgg11(pretrained=True)
+    model.classifier[6] = torch.nn.Linear(4096, 133, bias=True)
+
+    # Load the stored model parameters.
+    model_path = os.path.join(model_dir, "model.pth")
+    with open(model_path, "rb") as f:
+        model.load_state_dict(torch.load(f))
+
+    model.to(device).eval()
+
+    print("Done loading model.")
+    return model
+
+
 def main(
     seed,
     resize,
