19bf0e7d13971586de0fab35de72706b6ee411e1
Alex Costa Machado <alexcmachado@gmail.com>
2022-02-05 11:44:08 -0300

    Create Configs class for configuration management

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,5 +1,4 @@
 import argparse
-import json
 import os
 import torch
 import timeit
@@ -11,6 +10,17 @@ import base64
 from train import train
 from loaders import get_loaders
 from model import get_model
+
+
+class Configs:
+    def __init__(self, args):
+        self.seed = args.seed
+        self.epochs = args.epochs
+        self.use_transfer = args.use_transfer
+        self.train_dir = args.train_dir
+        self.valid_dir = args.valid_dir
+        self.model_dir = args.model_dir
+        self.use_cuda = args.use_cuda
 
 
 def model_fn(model_dir):
@@ -86,58 +96,7 @@ def predict_fn(data, model):
     return index
 
 
-def main(seed, epochs, use_transfer, train_dir, valid_dir, model_dir, use_cuda):
-
-    print("Use cuda: {}.".format(use_cuda))
-
-    torch.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-
-    # Load the training data.
-    loaders = get_loaders(train_dir, valid_dir)
-
-    # Build the model.
-    model = get_model(use_transfer)
-
-    params_to_update = []
-    for name, param in model.named_parameters():
-        if param.requires_grad == True:
-            params_to_update.append(param)
-
-    if use_cuda:
-        model.cuda()
-
-    print("Model loaded")
-
-    # Train the model.
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(params_to_update, lr=0.001, momentum=0.9)
-
-    model_path = os.path.join(model_dir, "model.pth")
-
-    start_time = timeit.default_timer()
-
-    model = train(epochs, loaders, model, optimizer, criterion, use_cuda, model_path)
-
-    end_time = timeit.default_timer()
-    t_sec = end_time - start_time
-    (t_min, t_sec) = divmod(t_sec, 60)
-    (t_hour, t_min) = divmod(t_min, 60)
-    print(
-        "Total training time: {:.0f} h, {:.0f} min, {:.0f} sec".format(
-            t_hour, t_min, t_sec
-        )
-    )
-
-    model_info_path = os.path.join(model_dir, "model_info.pth")
-    with open(model_info_path, "wb") as f:
-        model_info = {"use_transfer": use_transfer}
-        torch.save(model_info, f)
-
-
-if __name__ == "__main__":
-
+def parser_cli() -> Configs:
     parser = argparse.ArgumentParser()
 
     # Training Parameters
@@ -173,13 +132,73 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+    return Configs(args)
 
-    main(
-        args.seed,
-        args.epochs,
-        args.use_transfer,
-        args.train_dir,
-        args.valid_dir,
-        args.model_dir,
-        args.use_cuda,
+
+def write_model_to_disk(path: str, use_transfer: bool) -> None:
+    model_info_path = os.path.join(path, "model_info.pth")
+    with open(model_info_path, "wb") as f:
+        model_info = {"use_transfer": use_transfer}
+        torch.save(model_info, f)
+
+
+def run(configs: Configs) -> None:
+    print("Use cuda: {}.".format(configs.use_cuda))
+
+    torch.manual_seed(configs.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    # Load the training data.
+    loaders = get_loaders(configs.train_dir, configs.valid_dir)
+
+    # Build the model.
+    model = get_model(configs.use_transfer)
+
+    params_to_update = []
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            params_to_update.append(param)
+
+    if configs.use_cuda:
+        model.cuda()
+
+    print("Model loaded")
+
+    # Train the model.
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+
+    model_path = os.path.join(configs.model_dir, "model.pth")
+
+    start_time = timeit.default_timer()
+
+    model = train(
+        configs.epochs,
+        loaders,
+        model,
+        optimizer,
+        criterion,
+        configs.use_cuda,
+        model_path,
     )
+
+    end_time = timeit.default_timer()
+    t_sec = end_time - start_time
+    (t_min, t_sec) = divmod(t_sec, 60)
+    (t_hour, t_min) = divmod(t_min, 60)
+    print(
+        "Total training time: {:.0f} h, {:.0f} min, {:.0f} sec".format(
+            t_hour, t_min, t_sec
+        )
+    )
+    write_model_to_disk()
+
+
+def main() -> None:
+    configs = parser_cli()
+    run(configs)
+
+
+if __name__ == "__main__":
+    main()
